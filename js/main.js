0c9ebce6e53e376983800d1859a985523e8a617c
Chanwoo KIM <chanwoo.kim@outlook.fr>
2021-03-01 15:44:33 +0100

    Ajout : function addCart

--- js/main.js
+++ js/main.js
@@ -43,10 +43,77 @@ async function allProductsList() {
     }
 }
 
+//Création de la description des produits (product.html)
+async function showProduct() {
+    productId = window.location.search.replace('?id=', '');
+    if(allProductsId.includes(productId) == false) {
+        console.log('Wrong product ID !');
+    }
+    else {
+        productInfo = await getProduct();
+        const div = document.createElement('div');
+        div.className = 'row my-5';
+        div.innerHTML = `
+        <div class="col-12 col-md-6 d-flex align-items-center"><img class="img-fluid rounded-lg" src="${productInfo.imageUrl}" /></div>
+        <div class="col-12 col-md-6">
+        <h1 class="h3 text-center my-2">${productInfo.name}</h1>
+        <p class="text-justify my-2">${productInfo.description}</p>
+        <form><label for="product-option">Lentille</label>
+        <select class="form-control" id="product-option">
+        <option>Choisissez une option</option></select></form>
+        <p class="text-right h4 my-2">${productInfo.price / 100} €</p>
+        <button type="submit" id="addCart" class="btn btn-primary btn-block"><i class="fas fa-shopping-basket text-light mr-3"></i>Ajoutez au panier</button>
+        </div>`;
+        document.getElementById('product-description').appendChild(div);
+        
+        for(let i = 0; productInfo.lenses.length > i; i++) {
+            const option = document.createElement('option');
+            document.getElementById('product-option').appendChild(option);
+            option.innerHTML = `
+            ${productInfo.lenses[i]}`;
+        }
+
+        const addCartButton = document.getElementById('addCart')
+        addCartButton.addEventListener("click", await addCart);
+    }
+}
+
+//Ajouter le product lors d'un clic sur le button "Ajoutez au panier"
+const cart = []; // test: {id : "5be1ed3f1c9d44000030b061", amount : 1}
+let findIdPlace = "";
+function addCart() {
+    if(allProductsId.includes(window.location.search.replace('?id=', '')) == false) {
+        alert("Désolé. Ce produit n'existe pas.");
+    }
+    else {
+        alert("Ce produit a été ajouté à votre panier");
+        if(cart == "") {
+            cart.push(`{id : ${window.location.search.replace('?id=', '')}, amount : 1}`);
+        }
+        else {
+            for(count in cart) {
+                if(cart[count].id.includes(window.location.search.replace('?id=', ''))) {
+                    findIdPlace = count;
+                }
+            }
+            if(findIdPlace == "") {
+                cart.push(`{id : ${window.location.search.replace('?id=', '')}, amount : 1}`);
+            }
+            else {
+                cart[findIdPlace].amount += 1;
+            }
+        }
+        localStorage.setItem("cartJson", JSON.stringify(cart));
+    }
+};
+
 getProduct().then(values => {
     for(value of values) {
         allProductsId.push(value._id);
     }
     if(document.getElementById('product-list') != null) {
         allProductsList();
+    } else if(window.location.pathname == "/product.html" && window.location.search.startsWith('?id=')) {
+        showProduct();
+    }
 });
